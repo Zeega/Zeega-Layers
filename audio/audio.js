1c67368f5035693a76bd20b98e4c2a1d8f488e93
Rick Waldron <waldron.rick@gmail.com>
2012-12-12 16:28:57 -0500

    Indents to 4 spaces, double quotes, etc: audio/audio.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- audio/audio.js
+++ audio/audio.js
@@ -1,35 +1,37 @@
 define([
-  'zeega',
-  'zeega_dir/plugins/layers/_layer/_layer',
-  'zeega_dir/plugins/layers/video/video'
+    "zeega",
+    "zeega_dir/plugins/layers/_layer/_layer",
+    "zeega_dir/plugins/layers/video/video"
 ],
 
 function( Zeega, _Layer, VideoLayer ){
 
-  var Layer = Zeega.module();
+    var Layer = Zeega.module();
 
-  Layer.Audio = _Layer.extend({
-    layerType : 'Audio',
-    defaultAttributes: {
-      'title': 'Audio Layer',
-      'url': 'none',
-      'left': 0,
-      'top': 0,
-      'height': 0,
-      'width': 0,
-      'volume': 0.5,
-      'cue_in': 0,
-      'cue_out': null,
-      'fade_in': 0,
-      'fade_out': 0,
-      'opacity': 0,
-      'citation': true
-    }
-  });
+    Layer.Audio = _Layer.extend({
 
-  Layer.Audio.Visual = VideoLayer.Video.Visual.extend({
-    template: 'plugins/audio'
-  });
+        layerType: "Audio",
 
-  return Layer;
+        defaultAttributes: {
+            "title": "Audio Layer",
+            "url": "none",
+            "left": 0,
+            "top": 0,
+            "height": 0,
+            "width": 0,
+            "volume": 0.5,
+            "cue_in": 0,
+            "cue_out": null,
+            "fade_in": 0,
+            "fade_out": 0,
+            "opacity": 0,
+            "citation": true
+        }
+    });
+
+    Layer.Audio.Visual = VideoLayer.Video.Visual.extend({
+        template: "plugins/audio"
+    });
+
+    return Layer;
 });
