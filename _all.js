05190e5fa6135e8f5a7085f445469981c7c28c10
Rick Waldron <waldron.rick@gmail.com>
2012-12-12 14:28:52 -0500

    Indents to 4 spaces, double quotes, etc: _all.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- _all.js
+++ _all.js
@@ -7,31 +7,30 @@ this should be auto generated probably!!
 */
 
 define([
-  'zeega_dir/plugins/layers/image/image',
-  'zeega_dir/plugins/layers/link/link',
-  'zeega_dir/plugins/layers/slideshow/slideshow',
-  'zeega_dir/plugins/layers/video/video',
-  'zeega_dir/plugins/layers/audio/audio',
-  'zeega_dir/plugins/layers/rectangle/rectangle'
+    "zeega_dir/plugins/layers/image/image",
+    "zeega_dir/plugins/layers/link/link",
+    "zeega_dir/plugins/layers/slideshow/slideshow",
+    "zeega_dir/plugins/layers/video/video",
+    "zeega_dir/plugins/layers/audio/audio",
+    "zeega_dir/plugins/layers/rectangle/rectangle"
 ],
-  function(
+function(
     image,
     link,
     slideshow,
     video,
     audio,
     rectangle
-  ) {
+) {
     var Plugins = {};
     // extend the plugin object with all the layers
     return _.extend(
-      Plugins,
-      image,
-      link,
-      slideshow,
-      video,
-      audio,
-      rectangle
+        Plugins,
+        image,
+        link,
+        slideshow,
+        video,
+        audio,
+        rectangle
     );
-  }
-);
+});
