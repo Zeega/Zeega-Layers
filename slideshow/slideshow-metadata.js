0a19ae3a0dda0b0b07fdc37a26a61c082fe556f6
Joseph Bergen <jbergen@gmail.com>
2012-12-13 18:57:30 -0500

    added slideshow files

---
+++ slideshow/slideshow-metadata.js
@@ -0,0 +1,34 @@
+define([
+    "zeega",
+    "zeega_dir/plugins/layers/_layer/_layer"
+],
+
+function( Zeega, _Layer ) {
+
+    var Metadata = {};
+
+    Metadata.View = _Layer.LayoutView.extend({
+
+        className: 'slide-metadata',
+
+        template: "plugins/slideshow-metadata",
+
+        initialize: function() {
+            this.model.on('all', function(e) { console.log('e',e); });
+            this.model.on("slideshow_update", this.onUpdate, this );
+            this.slideInfo = this.model.get('attr').slides[0].attr;
+        },
+
+        onUpdate: function( info ) {
+            this.slideInfo = info.data.attr;
+            this.render();
+        },
+
+        serialize: function() {
+            return _.extend({ title: '', description: ''}, this.slideInfo );
+        }
+
+    });
+
+  return Metadata;
+});
