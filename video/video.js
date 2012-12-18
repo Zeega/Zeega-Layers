2123801a0ed6e07fdca03dd4096dc9c0a1f3ed90
Joseph Bergen <jbergen@gmail.com>
2012-12-18 14:09:38 -0500

    updated time update event to work with status

--- video/video.js
+++ video/video.js
@@ -137,7 +137,7 @@ function( Zeega, _Layer, MediaPlayer ) {
                     duration: this.mediaPlayer.getDuration()
                 };
 
-                this.model.trigger("media_timeupdate", info );
+                this.model.status.emit("media_timeupdate", info );
                 if ( this.mediaPlayer.getCurrentTime() >= out ) {
                     this.onEnded();
                 }
